283beb6767bdf4c0699e05406bd3cdaf0f599eae
turtlesergio <turtlesergio@yandex.ru>
2020-04-24 13:49:09 +0500

    last fixes 3

--- Popov_XO_test/Popov_XO_test/PlayField.h
+++ Popov_XO_test/Popov_XO_test/PlayField.h
@@ -1,41 +1,41 @@
 #pragma once
 
-#include "iostream"
 #include "vector"
-#include "cassert"
-#include "stdafx.h"
 #include "initializer_list"
 
 class PlayField {
 public:
-	enum CellStatus { csEmpty, csCross, csNought };
-	enum FieldStatus { fsInvalid, fsNormal, fsCrossesWin, fsNoughtsWin, fsDraw };
-	
-	class CellPos {
-	public:
-		CellPos GetCellPos(std::initializer_list<int> cell) {
-			return CellPos(cell);
-		}
-		void SetCellPos(int x, int y) {
-			_x = x;
-			_y = y;
-		}
-		CellPos(std::initializer_list<int> cell) {
-			_x = *cell.begin();
-			_y = *(cell.begin() + 1);
-		}
-		const int getX() { return _x; }
-		const int getY() { return _y; }
-	private:
-		int _x, _y;
-	};
+    enum CellStatus { csEmpty, csCross, csNought };
+    enum FieldStatus { fsInvalid, fsNormal, fsCrossesWin, fsNoughtsWin, fsDraw };
 
-	PlayField makeMove(CellPos cell) const;
-	std::vector<CellPos> getEmptyCells() const;
-	FieldStatus checkFieldStatus() const;
-	CellStatus operator[](CellPos cell);
+    class CellPos {
+    public:
+        CellPos GetCellPos(std::initializer_list<int> cell) {
+            return CellPos(cell);
+        }
+        void SetCellPos(int x, int y) {
+            _x = x;
+            _y = y;
+        }
+        CellPos(std::initializer_list<int> cell) {
+            _x = *cell.begin();
+            _y = *(cell.begin() + 1);
+        }
+        const int getX() { return _x; }
+        const int getY() { return _y; }
+    private:
+        int _x, _y;
+    };
+
+    PlayField makeMove(CellPos cell) const;
+    std::vector<CellPos> getEmptyCells() const;
+    FieldStatus checkFieldStatus() const;
+
+    //FieldStatus checkFieldStatusOLD() const;
+
+    CellStatus operator[](CellPos cell) const;
 private:
-	CellStatus playboard[3][3] = { csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, };
-	PlayField operator+(CellPos cell);
-	CellStatus nextMove() const;
+    CellStatus playboard[3][3] = { csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, csEmpty, };
+    PlayField operator+(CellPos cell) const;
+    CellStatus nextMove() const;
 };
--- Popov_XO_test/Popov_XO_test/TreeNode.cpp
+++ Popov_XO_test/Popov_XO_test/TreeNode.cpp
@@ -1,35 +1,37 @@
-#include "stdafx.h"
 #include "TreeNode.h"
+#include "cassert"
 
 TreeNode::TreeNode(PlayField newBoard, TreeNode* parent) : playboard(newBoard), parent(parent)
 {
 }
 
+TreeNode::~TreeNode() {
+    children.clear();
+}
+
 bool TreeNode::isTerminal() const {
-	assert(playboard.checkFieldStatus() != PlayField::fsInvalid);
-	return playboard.checkFieldStatus() == PlayField::fsNoughtsWin ||
-		playboard.checkFieldStatus() == PlayField::fsCrossesWin ||
-		playboard.checkFieldStatus() == PlayField::fsDraw;
+    assert(playboard.checkFieldStatus() != PlayField::fsInvalid);
+    return playboard.checkFieldStatus() != PlayField::fsNormal;
 }
 
 void TreeNode::addChild(TreeNode* child) {
-	assert(children.size() < 10);
-	child->parent = this;
-	children.push_back(child);
+    assert(children.size() <= child->childQty());
+    child->parent = this;
+    children.push_back(child);
 }
 
 TreeNode& TreeNode::operator[](const int index) const {
-	return *children[index];
+    return *children[index];
 }
 
 const PlayField& TreeNode::value() const {
-	return playboard;
+    return playboard;
 }
 
 int TreeNode::childCount() const {
-	return children.size();
+    return children.size();
 }
 
 int TreeNode::childQty() const {
-	return (parent ? parent->childQty() - 1 : 9);
+    return (parent ? parent->childQty() - 1 : 9);
 }
